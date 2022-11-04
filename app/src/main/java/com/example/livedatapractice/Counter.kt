f2fd76781396830a3fd686e45934abf83e091770
scarstreet <71519078+scarstreet@users.noreply.github.com>
2022-11-05 01:49:25 +0800

    I forgot these

--- app/src/main/java/com/example/livedatapractice/Counter.kt
+++ app/src/main/java/com/example/livedatapractice/Counter.kt
@@ -1,4 +1,16 @@
 package com.example.livedatapractice
 
-class Counter {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class Counter(lastValue:Int): ViewModel() {
+    val counter = MutableLiveData<Int>(lastValue)
+
+    fun addOne() {
+        val cnt = counter.value ?: 0
+        counter.value = cnt + 1
+    }
+    fun reset() {
+        counter.value = 0
+    }
 }
--- app/src/main/java/com/example/livedatapractice/MainActivity.kt
+++ app/src/main/java/com/example/livedatapractice/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.livedatapractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.livedatapractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+//    private lateinit var viewModel: Counter
+
+//    var counter:Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        viewModel = ViewModelProvider(this).get(Counter::class.java)
+//        binding.counterModel = viewModel
+//        binding.lifecycleOwner = this
+
+
+        var counter = Counter(0)
+
+        binding.addBtn.setOnClickListener{ counter.addOne() }
+        binding.resetBtn.setOnClickListener() { counter.reset() }
+        counter.counter.observe(this) {
+            cnt -> binding.countText.text = cnt.toString()
+        }
     }
 }
